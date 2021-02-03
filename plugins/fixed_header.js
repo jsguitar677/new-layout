2494977de1d28f874b430781340fe3e2ceef4dea
jsguitar677 <jsguitar677@gmail.com>
2021-02-04 00:45:26 +0800

    div switch
    switch desktop div  and mobile div bu using meadia query

---
+++ plugins/fixed_header.js
@@ -0,0 +1,15 @@
+const showAnim = gsap.from('.main_header', {
+    yPercent: -100,
+    paused: true,
+    duration: 0.2,
+
+}).progress(1);
+
+ScrollTrigger.create({
+
+    start: "top top",
+    // end: 99999,
+    onUpdate: (self) => {
+        self.direction === -1 ? showAnim.play() : showAnim.reverse()
+    }
+});
